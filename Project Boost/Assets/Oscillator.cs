6a1b09801c0796a8df01d965154c2fc24fee86bf
Levi Lais <levilais@gmail.com>
2018-06-04 17:35:37 -0700

    project boost update

--- Project Boost/Assets/Oscillator.cs
+++ Project Boost/Assets/Oscillator.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
-
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
 
     // todo remove from inspector later
     [Range(0, 1)] [SerializeField] float movementFactor; // 0 for not moved, 1 for fully moved.
@@ -20,6 +20,17 @@ public class Oscillator : MonoBehaviour {
 	
 	// Update is called once per frame
     void Update () {
+        if (period == 0f)
+
+
+
+        // todo protect against period is zero
+        float cycles = Time.time / period; // grows continually from 0
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(cycles * tau); // goes from -1 to +1
+
+        movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementFactor * movementVector;
         transform.position = startingPos + offset;
 	}
